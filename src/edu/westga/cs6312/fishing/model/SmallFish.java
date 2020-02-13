b3d208d4c846acf0eb3435a400acbf970a69a18c
J. Allen Burton <jburton6@my.westga.edu>
2020-02-13 13:58:50 -0500

    Created SmallFish class and methods

---
+++ src/edu/westga/cs6312/fishing/model/SmallFish.java
@@ -0,0 +1,72 @@
+package edu.westga.cs6312.fishing.model;
+
+/**
+ * This class defines a subclass of FishType which are small
+ * 
+ * @author J. Allen Burton
+ * @version Feb 13, 2020
+ *
+ */
+
+public class SmallFish extends FishType {
+
+	/**
+	 * Constructor for SmallFish objects which initializes the number of fish in
+	 * their school to 100
+	 *
+	 * @precondition none
+	 *
+	 * @postcondition new SmallFish object (subclass of FishType) which has 100 fish
+	 *                in the school
+	 */
+	public SmallFish() {
+		super(100);
+	}
+
+	/**
+	 * This method provides a way to catch a fixed number of fish which removes
+	 * that number of fish from the school.  Currently set to a fixed value of 50 
+	 * fish caught.
+	 * 
+	 * @return	the number of fish removed from the school
+	 *
+	 * @precondition	none
+	 *
+	 * @postcondition	removes 50 fish from school
+	 */
+	@Override
+	public int catchFish() {
+		int numberOfFishCaught = 50;
+		super.removeFish(numberOfFishCaught);
+		return numberOfFishCaught;
+	}
+
+	/**
+	 * Method to get the cost to fish this type of fish.
+	 * 
+	 * @return the cost to fish this type of fish
+	 *
+	 * @precondition none
+	 *
+	 * @postcondition no change to object
+	 */
+	@Override
+	public int costToFish() {
+		return 25;
+	}
+	
+	/**
+	 * This method returns a string representation of this SmallFish object
+	 * 
+	 * @return	string representation of this SmallFish object
+	 *
+	 * @precondition	none	
+	 *
+	 * @postcondition	no change to object
+	 */
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+}
